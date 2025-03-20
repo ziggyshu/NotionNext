8bbda6e28fcce66f1fc701bff758ba6c6e536a4e
ziggyshu <120007282+ziggyshu@users.noreply.github.com>
2025-03-20 19:53:56 +0800

    qqq

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Tianheng Shu', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'A PhD candidate at Tsinghua University', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || ' ', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://shuth.netlify.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'academic, blog', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
