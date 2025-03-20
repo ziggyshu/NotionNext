d24cbceec460ada7ecaabeb32efd39a221c686c3
ziggyshu <120007282+ziggyshu@users.noreply.github.com>
2025-03-20 20:49:22 +0800

    aaaaaaaaa

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -7,7 +7,7 @@ const Logo = props => {
     <Link href='/' passHref legacyBehavior>
       <div
         className={
-          'flex flex-col justify-center items-center cursor-pointer bg-black dark:bg-gray-800 space-y-3 font-bold ' +
+          'flex flex-col justify-center items-center cursor-pointer bg-black dark:bg-gray-300 space-y-3 font-bold ' +
           className
         }>
         <div
@@ -15,7 +15,7 @@ const Logo = props => {
           data-aos-duration='500'
           data-aos-once='true'
           data-aos-anchor-placement='top-bottom'
-          className='font-serif text-xl text-white logo'>
+          className='font-light text-xl text-white logo'>
           {' '}
           {siteConfig('TITLE')}
         </div>
@@ -25,7 +25,7 @@ const Logo = props => {
           data-aos-delay='300'
           data-aos-once='true'
           data-aos-anchor-placement='top-bottom'
-          className='text-sm text-gray-300 font-light text-center'>
+          className='text-sm text-gray-300 font-sans text-center'>
           {' '}
           {siteConfig('DESCRIPTION')}
         </div>
