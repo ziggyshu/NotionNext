2e46d840bfcc9f1a018fc94658b2c69a6d80cba1
ziggyshu <120007282+ziggyshu@users.noreply.github.com>
2025-03-21 10:10:47 +0800

    card

--- themes/next/components/BlogPostCard.js
+++ themes/next/components/BlogPostCard.js
@@ -26,109 +26,76 @@ const BlogPostCard = ({ post, index, showSummary }) => {
       : {}
 
   return (
-    <Card className='w-full'>
-      <div
-        key={post.id}
-        className='flex flex-col-reverse justify-between duration-300'>
-        <div className='lg:p-8 p-4 flex flex-col w-full'>
-          {/* 文章标题 */}
-          <Link
-            {...aosProps}
-            href={post?.href}
-            passHref
-            className={`cursor-pointer text-3xl ${showPreview ? 'text-center' : ''} leading-tight text-gray-700 dark:text-gray-100 hover:text-blue-500 dark:hover:text-blue-400`}>
-            {siteConfig('POST_TITLE_ICON') && (
-              <NotionIcon icon={post.pageIcon} />
-            )}{' '}
-            <span className='menu-link'>{post.title}</span>
-          </Link>
-
-          <div
-            {...aosProps}
-            className={`flex mt-2 items-center ${showPreview ? 'justify-center' : 'justify-start'} flex-wrap dark:text-gray-500 text-gray-500 `}>
-            <div>
-              {post.category && (
-                <>
-                  <Link
-                    href={`/category/${post.category}`}
-                    passHref
-                    className='hover:text-blue-500 dark:hover:text-blue-400 cursor-pointer font-light text-sm transform'>
-                    <i className='mr-1 fas fa-folder' />
-                    <span className='menu-link'>{post.category}</span>
-                  </Link>
-                  <span className='mx-2'>|</span>
-                </>
-              )}
-              <Link
-                href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}
-                passHref
-                className='hover:text-blue-500 dark:hover:text-blue-400 font-light cursor-pointer text-sm leading-4 mr-3'>
-                <span className='menu-link'>{post.date?.start_date}</span>
-              </Link>
-            </div>
-
-            <TwikooCommentCount
-              post={post}
-              className='hover:text-blue-500 dark:hover:text-blue-400 hover:underline text-sm'
+<Card className='w-full mb-1'>
+  <div className='flex flex-row justify-between duration-300'>
+    {/* 封面图 - 左侧 */}
+    {siteConfig('NEXT_POST_LIST_COVER', null, CONFIG) &&
+      post?.pageCoverThumbnail && (
+        <Link href={post?.href} passHref legacyBehavior>
+          <div className='w-1/3 h-24 relative duration-200 cursor-pointer transform overflow-hidden'>
+            <Image
+              className='hover:scale-105 transform duration-500'
+              src={post?.pageCoverThumbnail}
+              alt={post.title}
+              layout='fill'
+              objectFit='cover'
+              loading='lazy'
             />
-
-            <div className='hover:text-blue-500 dark:hover:text-blue-400  md:flex-nowrap flex-wrap md:justify-start inline-block'>
-              {post.tagItems?.map(tag => (
-                <TagItemMini key={tag.name} tag={tag} />
-              ))}
-            </div>
           </div>
+        </Link>
+      )}
 
-          {(!showPreview || showSummary) && !post.results && (
-            <p
-              {...aosProps}
-              className='mt-4 mb-12 text-gray-700 dark:text-gray-300 text-sm font-light leading-7'>
-              {post.summary}
-            </p>
-          )}
+    {/* 内容 - 右侧 */}
+    <div className='flex-1 lg:p-3 p-1 flex flex-col'>
+      {/* 文章标题 */}
+      <Link
+        {...aosProps}
+        href={post?.href}
+        passHref
+        className={`cursor-pointer text-lg ${showPreview ? 'text-center' : ''} leading-tight text-gray-700 dark:text-gray-100 hover:text-blue-500 dark:hover:text-blue-400`}>
+        {siteConfig('POST_TITLE_ICON') && (
+          <NotionIcon icon={post.pageIcon} />
+        )}{' '}
+        <span className='menu-link'>{post.title}</span>
+      </Link>
 
-          {/* 搜索结果 */}
-          {post.results && (
-            <p className='line-clamp-4 mt-4 text-gray-700 dark:text-gray-300 text-sm font-light leading-7'>
-              {post.results.map((r, index) => (
-                <span key={index}>{r}</span>
-              ))}
-            </p>
-          )}
-
-          {showPreview && post?.blockMap && (
-            <div className='overflow-ellipsis truncate'>
-              <NotionPage post={post} />
-            </div>
-          )}
-
-          <div className='text-right border-t pt-8 border-dashed'>
-            <Link
-              href={post?.href}
-              className='hover:bg-opacity-100 hover:underline transform duration-300 p-2 text-white bg-gray-800 cursor-pointer'>
-              {locale.COMMON.ARTICLE_DETAIL}
-              <i className='ml-1 fas fa-angle-right' />
-            </Link>
-          </div>
-        </div>
-
-        {siteConfig('NEXT_POST_LIST_COVER', null, CONFIG) &&
-          post?.pageCoverThumbnail && (
-            <Link href={post?.href} passHref legacyBehavior>
-              <div className='h-72 w-full relative duration-200 cursor-pointer transform overflow-hidden'>
-                <Image
-                  className='hover:scale-105 transform duration-500'
-                  src={post?.pageCoverThumbnail}
-                  alt={post.title}
-                  layout='fill'
-                  objectFit='cover'
-                  loading='lazy'
-                />
-              </div>
-            </Link>
-          )}
+      {/* 日期和标签 */}
+      <div
+        {...aosProps}
+        className={`flex mt-0.5 items-center ${showPreview ? 'justify-center' : 'justify-start'} flex-wrap dark:text-gray-500 text-gray-500 leading-4`}>
+        {/* ... 其他内容 ... */}
       </div>
-    </Card>
+
+      {/* 摘要 */}
+      {(!showPreview || showSummary) && !post.results && (
+        <p
+          {...aosProps}
+          className='mt-1 mb-2 text-gray-700 dark:text-gray-300 text-sm font-light leading-4'>
+          {post.summary}
+        </p>
+      )}
+
+      {/* 搜索结果 */}
+      {post.results && (
+        <p className='line-clamp-1 mt-1 text-gray-700 dark:text-gray-300 text-sm font-light leading-4'>
+          {post.results.map((r, index) => (
+            <span key={index}>{r}</span>
+          ))}
+        </p>
+      )}
+
+      {/* 按钮区域 */}
+      <div className='text-right border-t pt-1 border-dashed'>
+        <Link
+          href={post?.href}
+          className='hover:bg-opacity-100 hover:underline transform duration-300 px-1 py-0.5 text-xs text-white bg-gray-800 cursor-pointer'>
+          {locale.COMMON.ARTICLE_DETAIL}
+          <i className='ml-1 fas fa-angle-right text-xs' />
+        </Link>
+      </div>
+    </div>
+  </div>
+</Card>
   )
 }
 
