08defbb73bde3a50572865caa308daef97689d76
ziggyshu <120007282+ziggyshu@users.noreply.github.com>
2025-03-21 10:45:05 +0800

    bb

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -7,7 +7,7 @@ const Logo = props => {
     <Link href='/' passHref legacyBehavior>
       <div
         className={
-          'flex flex-col justify-center items-center cursor-pointer bg-black dark:bg-gray-300 space-y-3 font-bold ' +
+          'flex flex-col justify-center items-center cursor-pointer bg-[#a49e97] space-y-3 font-bold ' +
           className
         }>
         <div
