7cf2796a62795cafb03b974be3a47af2e4352960
ziggyshu <120007282+ziggyshu@users.noreply.github.com>
2025-06-08 23:53:38 +0800

    55

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || ' ', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tianheng-shu.cn/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'academic, blog', // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'sthcqu@163.com', // 邮箱地址 例如mail@tangly1024.com
